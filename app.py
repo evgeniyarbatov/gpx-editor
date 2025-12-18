78c9c35f46c8aeb3a86b0752e2f3eb4dc58a47fb
Evgeny Arbatov <arbatovevgeny@gmail.com>
2025-12-18 11:33:39 +0700

    chore: format python code with black

--- app.py
+++ app.py
@@ -5,58 +5,60 @@ import streamlit as st
 
 from utils import reverse_gpx, split_gpx
 
+
 def create_download_link(gpx, distance):
     b64 = base64.b64encode(gpx.encode()).decode()
     href = f'<a href="data:application/octet-stream;base64,{b64}" download="{distance}km.gpx">{distance}km.gpx</a>'
     return href
 
+
 def render_gpx_slits(gpx, points_per_file, start_lat, start_lng):
-  gpx_segments = split_gpx(gpx, points_per_file, start_lat, start_lng)
-  for distance, gpx_segment in gpx_segments:
-    st.markdown(create_download_link(gpx_segment, distance), unsafe_allow_html=True)
-  
-st.title('GPX Editor')
+    gpx_segments = split_gpx(gpx, points_per_file, start_lat, start_lng)
+    for distance, gpx_segment in gpx_segments:
+        st.markdown(create_download_link(gpx_segment, distance), unsafe_allow_html=True)
+
+
+st.title("GPX Editor")
 
 uploaded_file = st.file_uploader("GPX file", type=["gpx"])
 points_per_file = st.selectbox("How many points per file?", [200, 500, 1000], index=0)
 
 if uploaded_file is not None:
-  gpx_file = uploaded_file.read()
-  gpx = gpxpy.parse(gpx_file)
+    gpx_file = uploaded_file.read()
+    gpx = gpxpy.parse(gpx_file)
 
-  reverse_choice = st.radio(
-      "Reverse direction?",
-      ("Yes", "No"),
-      index=1,
-  )
-  if reverse_choice == "Yes":
-    gpx = reverse_gpx(gpx)
-    st.success("GPX file reversed")
-  else:
-    st.info("GPX file NOT reversed")
+    reverse_choice = st.radio(
+        "Reverse direction?",
+        ("Yes", "No"),
+        index=1,
+    )
+    if reverse_choice == "Yes":
+        gpx = reverse_gpx(gpx)
+        st.success("GPX file reversed")
+    else:
+        st.info("GPX file NOT reversed")
 
-  start_choice = st.radio(
-      "Start from the beginning of the GPX file?",
-      ("Yes", "No"),
-      index=0,
-  )
-  start_lat, start_lng = None, None
-  if start_choice == "Yes":
-      st.info("Using GPX starting point")
-  else:
-      start_lat = st.number_input(
-        "Start latitude:", 
-        value=1.3097970339490435, 
-        format="%.6f",
-        step=0.000001,
-      )
-      start_lng = st.number_input(
-        "Start longitude:", 
-        value=103.89455470068188, 
-        format="%.6f",
-        step=0.000001,
-      )
-      st.success("Using custom starting point")
-  
-  render_gpx_slits(gpx, points_per_file, start_lat, start_lng)
+    start_choice = st.radio(
+        "Start from the beginning of the GPX file?",
+        ("Yes", "No"),
+        index=0,
+    )
+    start_lat, start_lng = None, None
+    if start_choice == "Yes":
+        st.info("Using GPX starting point")
+    else:
+        start_lat = st.number_input(
+            "Start latitude:",
+            value=1.3097970339490435,
+            format="%.6f",
+            step=0.000001,
+        )
+        start_lng = st.number_input(
+            "Start longitude:",
+            value=103.89455470068188,
+            format="%.6f",
+            step=0.000001,
+        )
+        st.success("Using custom starting point")
 
+    render_gpx_slits(gpx, points_per_file, start_lat, start_lng)
--- utils.py
+++ utils.py
@@ -3,26 +3,32 @@ from geopy.distance import geodesic
 import xml.dom.minidom
 from xml.etree.ElementTree import Element, SubElement, tostring
 
+
 def format_gpx(gpx, distance):
-  gpx_file = xml.dom.minidom.parseString(
-    tostring(gpx, encoding="unicode")
-  ).toprettyxml()
+    gpx_file = xml.dom.minidom.parseString(
+        tostring(gpx, encoding="unicode")
+    ).toprettyxml()
 
-  distance = round(distance / 1000.0)
+    distance = round(distance / 1000.0)
 
-  return (distance, gpx_file)
+    return (distance, gpx_file)
+
 
 def create_gpx():
-  gpx = Element('gpx', {
-      'creator': 'StravaGPX',
-      'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-      'xsi:schemaLocation': 'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd',
-      'version': '1.1',
-      'xmlns': 'http://www.topografix.com/GPX/1/1'
-  })
-  trk = SubElement(gpx, "trk")
-  trkseg = SubElement(trk, "trkseg")
-  return gpx, trkseg
+    gpx = Element(
+        "gpx",
+        {
+            "creator": "StravaGPX",
+            "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+            "xsi:schemaLocation": "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd",
+            "version": "1.1",
+            "xmlns": "http://www.topografix.com/GPX/1/1",
+        },
+    )
+    trk = SubElement(gpx, "trk")
+    trkseg = SubElement(trk, "trkseg")
+    return gpx, trkseg
+
 
 def reverse_gpx(gpx):
     for track in gpx.tracks:
@@ -30,65 +36,69 @@ def reverse_gpx(gpx):
             segment.points.reverse()
     return gpx
 
+
 def find_closest_point(
-  gpx,
-  start_lat=None,
-  start_lng=None,
+    gpx,
+    start_lat=None,
+    start_lng=None,
 ):
-  closest_point = None
-  min_distance = float('inf')
-  for track in gpx.tracks:
-    for segment in track.segments:
-      for point in segment.points:
-        distance = geodesic((point.latitude, point.longitude), (start_lat, start_lng)).meters
-        if distance < min_distance:
-          min_distance = distance
-          closest_point = point
-  return str(closest_point.latitude), str(closest_point.longitude)
+    closest_point = None
+    min_distance = float("inf")
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                distance = geodesic(
+                    (point.latitude, point.longitude), (start_lat, start_lng)
+                ).meters
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_point = point
+    return str(closest_point.latitude), str(closest_point.longitude)
+
 
 def split_gpx(
-  gpx, 
-  points_per_file=200,
-  start_lat=None,
-  start_lng=None,
+    gpx,
+    points_per_file=200,
+    start_lat=None,
+    start_lng=None,
 ):
-  split_gpx_files = []
-  distance = 0
-  gpx_out, trkseg_out = create_gpx()
+    split_gpx_files = []
+    distance = 0
+    gpx_out, trkseg_out = create_gpx()
 
-  route_started = True
-  if start_lat != None and start_lng != None:
-    route_started = False
-    start_lat, start_lng = find_closest_point(gpx, start_lat, start_lng)
+    route_started = True
+    if start_lat != None and start_lng != None:
+        route_started = False
+        start_lat, start_lng = find_closest_point(gpx, start_lat, start_lng)
 
-  point_count = 0
-  prev_lat, prev_lng = None, None
+    point_count = 0
+    prev_lat, prev_lng = None, None
 
-  for track in gpx.tracks:
-    for segment in track.segments:
-      for point in segment.points:
-        lat, lng = str(point.latitude), str(point.longitude)
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                lat, lng = str(point.latitude), str(point.longitude)
 
-        if start_lat != None and start_lng != None:
-          if lat == start_lat and lng == start_lng:
-            route_started = True
+                if start_lat != None and start_lng != None:
+                    if lat == start_lat and lng == start_lng:
+                        route_started = True
 
-        if not route_started:
-          continue
+                if not route_started:
+                    continue
 
-        if point_count > 0:
-          if prev_lat != None and prev_lng != None:
-            distance += geodesic((prev_lat, prev_lng), (lat, lng)).meters
-  
-          if point_count % points_per_file == 0:
-            split_gpx_files.append(format_gpx(gpx_out, distance))
-            gpx_out, trkseg_out = create_gpx()
+                if point_count > 0:
+                    if prev_lat != None and prev_lng != None:
+                        distance += geodesic((prev_lat, prev_lng), (lat, lng)).meters
 
-        SubElement(trkseg_out, "trkpt", attrib={"lat": lat, "lon": lng})
+                    if point_count % points_per_file == 0:
+                        split_gpx_files.append(format_gpx(gpx_out, distance))
+                        gpx_out, trkseg_out = create_gpx()
 
-        prev_lat, prev_lng = lat, lng
-        point_count += 1
+                SubElement(trkseg_out, "trkpt", attrib={"lat": lat, "lon": lng})
 
-  split_gpx_files.append(format_gpx(gpx_out, distance))
+                prev_lat, prev_lng = lat, lng
+                point_count += 1
 
-  return split_gpx_files
+    split_gpx_files.append(format_gpx(gpx_out, distance))
+
+    return split_gpx_files
